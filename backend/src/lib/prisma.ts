d8aefe5c9f6d217410be52aacaf727603f7a8dc3
Rishab raj <rishabraj2211@gmail.com>
2026-06-25 04:06:08 +0530

    removed unnecessary code

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -1,16 +1,3 @@
-// import { PrismaClient } from '../../src/generated/prisma'
-
-// declare global {
-//     var prisma : PrismaClient | undefined
-// }
-
-// export const client = globalThis.prisma || new PrismaClient({
-//     accelerateUrl : process.env.DB_ACCELERATE_URL!
-// })
-
-// if(process.env.NODE_ENV !== 'production') globalThis.prisma = client
-
-
 import { PrismaClient } from '../../src/generated/prisma'
 
 declare global {
